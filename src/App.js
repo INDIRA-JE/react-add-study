605cfbf0ccb451cfb2cfae6b995827cb2ada2cbf
JE <whddms87@gmail.com>
2023-04-10 20:23:21 +0900

    4. 일반 변수 vs useState 차이점

--- src/App.js
+++ src/App.js
@@ -3,41 +3,19 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  /* 
-    일반 변수 : 새로고침 해야 렌더
-    useState : 값이 변경되면 자동으로 렌더
-  */
+  let [num, setNum] = useState(0);
+  console.log("num : ", num);
 
-  // 1. useState를 사용한 name 내용 출력
-  // let [name, setName] = useState("JE");
-  // 2. usdState 필요성 : 버튼을 누를 때마다 증가값이 출력되길 원할 때
-  // let num = 1;  // 변경 해준다
-
-  // function numUp() {
-  //   num = num + 1;
-  //   console.log(num);
-  // }
-  // 2. useState 사용
-  let [num, setNum] = useState(1);
-  // 3. 1초마다 1씩 증가 (실행할 메소드, 시간)
-  setTimeout(() => setNum((num = num + 1)), 1000);
+  /**
+   * 일반변수 : 새로고침 해야 렌더
+   * useState : 값이 변경되면 자동으로 렌더
+   */
 
   return (
     <div className="App">
       <header className="App-header">
-        {/* 1. 
-        <div>{name}</div> */}
-        {/* 2. setNum : 실행되는 순간 렌더링이 되는 것을 알 수 있다*/}
-        {/* <div>{num}</div>
-        <button
-        onClick={() => {
-          setNum((num = num + 1));
-        }}
-        >
-        버튼
-      </button> */}
-        {/* 3.  */}
-        <div>{num}</div>
+        <div></div>
+        <button onClick={() => setNum((num = num + 1))}>버튼</button>
       </header>
     </div>
   );
